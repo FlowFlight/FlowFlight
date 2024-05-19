925af42676fa76f753dda32e3f76455a59d83236
gruszczrob <gruszczrob@gmail.com>
2024-05-19 12:23:19 +0200

    not working yet, have to add data normalization
    Co-authored-by: Jakub Janik <JamesYanik@users.noreply.github.com>

--- NeuralNetworkScript/neuralNetwork.py
+++ NeuralNetworkScript/neuralNetwork.py
@@ -2,10 +2,11 @@ import numpy as np
 
 class NeuralNetwork:
     def __init__(self, learning_rate):
-        self.weights = np.array([np.random.randn(), np.random.randn(), np.random.randn()])
-        self.bias = np.random.randn()
+        with open('weights.txt', 'r') as saves_file:
+            saves = [float(x) for x in saves_file.read().split('\n')]
+        self.weights = np.array(saves[0:-1])
+        self.bias = saves[-1]
         self.learning_rate = learning_rate
-
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
@@ -15,38 +16,44 @@ class NeuralNetwork:
     def predict(self, input_vector_array) -> list[float]:
         predictions_array = []
         for input_vector in input_vector_array:
-            layer_1 = np.dot(input_vector, self.weights) + self.bias
+            layer_1 = np.dot(input_vector, self.weights)/5 + self.bias
             layer_2 = self._sigmoid(layer_1)
             result = layer_2
             predictions_array.append(result)
         return predictions_array
 
-    def _compute_gradients(self, input_vectors, target):
-        # Calculate gradients for each input vector in the batch
+    def _compute_gradients(self, input_vectors, targets):
+        
         derror_dbias_batch = 0
         derror_dweights_batch = np.zeros_like(self.weights)
         
-        predictions_array = self.predict(input_vectors)
-        for input_vector in input_vectors:
+        for input_vector, target in zip(input_vectors, targets):
+            #zprint(input_vector)
+            #input_vector = np.array(input_vector)  # Convert to numpy array if it's not already
+            print(input_vector)
             layer_1 = np.dot(input_vector, self.weights) + self.bias
+            layer_2 = self._sigmoid(layer_1)
+            print(layer_1)
 
-            derror_dprediction = 2 * (predictions_array - target)
+
+            derror_dprediction = 2 * (layer_2 - target)
             dprediction_dlayer1 = self._sigmoid_deriv(layer_1)
             dlayer1_dbias = 1
             dlayer1_dweights = input_vector
 
             derror_dbias = derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
-            derror_dweights = derror_dprediction * dprediction_dlayer1 * dlayer1_dweights
-
+            derror_dweights = np.multiply(derror_dprediction * dprediction_dlayer1, dlayer1_dweights)
             # Accumulate gradients for the entire batch
             derror_dbias_batch += derror_dbias
-            derror_dweights_batch += derror_dweights
+            
+            derror_dweights_batch = np.add(derror_dweights, derror_dweights_batch)
 
         # Average the gradients for the batch
         derror_dbias_batch /= len(input_vectors)
         derror_dweights_batch /= len(input_vectors)
 
         return derror_dbias_batch, derror_dweights_batch
+
 
     def _update_parameters(self, derror_dbias, derror_dweights):
         self.bias = self.bias - (derror_dbias * self.learning_rate)
@@ -75,13 +82,18 @@ class NeuralNetwork:
                 cumulative_error = 0
                 # Loop through all the instances to measure the error
                 for data_instance_index in range(len(input_vectors)):
-                    data_point = input_vectors[data_instance_index]
-                    target = targets[data_instance_index]
+                    for vector_data_instance_index in range(len(input_vector[data_instance_index])):    
+                        data_point = input_vectors[data_instance_index]
+                        target = targets[data_instance_index][vector_data_instance_index]
 
-                    prediction = self.predict(data_point)
-                    error = np.square(prediction - target)
+                        prediction = self.predict(data_point)[data_instance_index]
+                        error = np.square(prediction - target)
 
-                    cumulative_error = cumulative_error + error
-                cumulative_errors.append(cumulative_error)
+                        cumulative_error = cumulative_error + error
+                    cumulative_errors.append(cumulative_error)
 
-        return cumulative_errors
+        with open('weights.txt', 'w') as saves_file:
+            for weight in self.weights:
+                saves_file.write(str(weight) + '\n')
+            saves_file.write(str(self.bias))
+        return cumulative_errors   
